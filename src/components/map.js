8cf9d6412e141e5533bc57aaf443e10745d84e24
Pavel Krajcevski <Krajcevski@gmail.com>
2019-02-06 20:52:38 -0800

    Use mapbox to show geoJSON on the client built from dynamic 311 data

--- src/components/map.js
+++ src/components/map.js
@@ -10,26 +10,98 @@ const kMaxBounds = new mapboxgl.LngLatBounds(
   new mapboxgl.LngLat(-122.28, 37.9)
 );
 
+function geoJSONFromBikeData(data) {
+  var result = {
+    "type": "FeatureCollection",
+    "features": []
+  };
+
+  for (var i = 0; i < data.length; ++i) {
+    var feature = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [data[i].point.longitude, data[i].point.latitude]
+      },
+      "properties": {
+        "title": "Point" + i,
+        "icon": "harbor",
+        "color": "#FFFFFF"
+      }
+    };
+    result.features.push(feature)
+  }
+
+  console.log(result);
+
+  return result;
+};
+
 export default class Map extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      bike_lane_reports: []
+    };
+  }
+  
   componentDidMount() {
-    this.initializeMap()
+    const base_url = 'https://data.sfgov.org/resource/ihm3-5gmc.json'
+    const url_filters = [['service_subtype', 'Blocking_Bicycle_Lane']]
+
+    var url = base_url;
+    for (var i = 0; i < url_filters.length; i++) {
+      var sep = '&';
+      if (i == 0) { sep = '?' }
+      url += sep + url_filters[i][0] + '=' + url_filters[i][1];
+    }
+    
+    fetch(url).then(results => { return results.json() }).then(data => {
+      this.initializeMap(data)
+    });
   }
 
   componentWillUnmount() {
     this.map.remove();
   }
 
-  initializeMap(){
+  initializeMap(bike_lane_data) {
     mapboxgl.accessToken =
       "pk.eyJ1IjoiYWdhZXNzZXIiLCJhIjoiY2pvZGY5bmh4MWJtcTNsbWtmN2RmNnhiNCJ9.iwOotv1u0S92o-Vj2CCjag";
 
     this.map = new mapboxgl.Map({
       container: kMapId,
-      style: "mapbox://styles/agaesser/cjn5lb26b0gty2rnr3laj0ljd", // stylesheet location
-      center: [-122.450577, 37.759108], // starting position [lng, lat]
-      zoom: kMinZoom, // Start all the way zoomed out.
+      // stylesheet location
+      style: "mapbox://styles/agaesser/cjn5lb26b0gty2rnr3laj0ljd",
+
+      // starting position [lng, lat]
+      center: [-122.450577, 37.759108],
+
+      // Start all the way zoomed out.
+      zoom: kMinZoom,
       minZoom: kMinZoom,
       maxBounds: kMaxBounds
+    });
+
+    this.map.on("load", () => {
+      this.map.addLayer({
+        "id": "points",
+        "type": "symbol",
+        "source": {
+          "type": "geojson",
+          "data": geoJSONFromBikeData(bike_lane_data)
+        },
+        "layout": {
+          "icon-image": "{icon}-15",
+          "text-field": "{title}",
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.6],
+          "text-anchor": "top",
+        },
+        "paint": {
+          "text-color": ['get', 'color']
+        }
+      });
     });
 
     this.map.on(
