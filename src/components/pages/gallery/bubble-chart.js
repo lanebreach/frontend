7715d1ae16c5dabc1abd056077b3bb8fcbc04710
yubozhao <yubz86@gmail.com>
2019-02-24 21:11:45 -0800

    add gallery page(data fetching switching charts)

---
+++ src/components/pages/gallery/bubble-chart.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const BubbleChart = ({data}) => {
+  return (
+    <div>
+      <h2>Bubble Chart</h2>
+      {JSON.stringify(data)}
+    </div>
+  );
+};
---
+++ src/components/pages/gallery/line-chart.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const LineChart = ({data}) => {
+  return (
+    <div>
+      <h2>Line Chart</h2>
+      {JSON.stringify(data)}
+    </div>
+  );
+};
