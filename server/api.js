d3002b51bf74a5cb44bff6f64292725e1576a7fe
Pavel Krajcevski <Krajcevski@gmail.com>
2019-03-20 21:05:16 -0700

    Change endpoint for bike lane reports to be our API

--- server/api.js
+++ server/api.js
@@ -3,23 +3,35 @@ import axios from "axios"
 
 const API = express.Router();
 
+function aMonthAgo(date) {
+  const day1 = Date.parse('25 Aug 2018 00:00:00 GMT')
+  const day2 = Date.parse('25 Jul 2018 00:00:00 GMT')
+  return new Date(date - (day1 - day2));
+}
+
 API.get("/reports",  async (req, res) => {
-  const base_url = 'https://data.sfgov.org/resource/ktji-gk7t.json'
-  const url_filters = [['service_subtype', 'Blocking_Bicycle_Lane']]
+  const base_url = 'https://lane-breach.herokuapp.com/api/sf311_cases'
+  var today = new Date();
+  const url_filters = [ ['start_time', aMonthAgo(today).toISOString().substring(0, 10)],
+                        ['end_time', today.toISOString().substring(0, 10)],
+                        ['per_page', 500]
+                      ]
   var url = base_url;
   for (var i = 0; i < url_filters.length; i++) {
     var sep = '&';
     if (i == 0) { sep = '?' }
     url += sep + url_filters[i][0] + '=' + url_filters[i][1];
   }
+
   const { data: bikeLaneReports } = await axios.get(url);
+
   const formattedData = {
     "type": "FeatureCollection",
-    "features": bikeLaneReports.map((report, i) => ({
+    "features": bikeLaneReports.data.map((report, i) => ({
       type: "Feature",
       geometry: {
         type: "Point",
-        coordinates: report.point.coordinates,
+        coordinates: [report.long, report.lat],
       },
       properties: {
         title: `Point${i}`,
