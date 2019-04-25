cefaf9976181cc3cd6c424460874714a2c843917
Pavel Krajcevski <Krajcevski@gmail.com>
2019-04-24 20:54:37 -0700

    Add About button that links to gallery page for now...

--- src/frontend/components/pages/Landing.js
+++ src/frontend/components/pages/Landing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Map from "../map";
+import { Redirect } from "react-router";
 
 const Padding = styled.div`
   padding: 20px;
@@ -14,9 +15,40 @@ const Title = styled.h1`
   color: wheat;
 `;
 
+class About extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      redirect: false
+    };
+  }
+
+  handleOnClick = () => {
+    this.setState({redirect: true});
+  }
+
+  render = () => {
+    if (this.state.redirect) {
+      return <Redirect push to="/gallery" />;
+    }
+
+    const style = {
+      "position": "relative",
+      "float": "right",
+      "paddingRight": "20px",
+      "zIndex": 1,
+      "color": "wheat",
+      "cursor": "pointer"
+    };
+
+    return (<span onClick={this.handleOnClick} style={style}><h4>About</h4></span>);
+  }
+};
+
 const Landing = () => (
   <Padding>
     <Title>Let's get cars out of bike lanes.</Title>
+    <About />
     <Map />
   </Padding>
 );
