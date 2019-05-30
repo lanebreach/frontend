2bf407bdd4581ac1d4ed47d1a213ed96bc051c15
Pavel Krajcevski <Krajcevski@gmail.com>
2019-05-29 20:17:08 -0700

    Fix popup clicks after switching to our API

--- src/frontend/common/fetchData.js
+++ src/frontend/common/fetchData.js
@@ -7,39 +7,50 @@ function aMonthAgo(date) {
 }
 
 
-  export async function fetch311() {
-    const base_url = 'https://lane-breach.herokuapp.com/api/sf311_cases'
-    var today = new Date();
-    const url_filters = [
-      ['start_time', aMonthAgo(today).toISOString().substring(0, 10)],
-      ['end_time', today.toISOString().substring(0, 10)],
-      ['per_page', 500]
-    ];
+export async function fetch311() {
+  const base_url = 'https://lane-breach.herokuapp.com/api/sf311_cases'
+  var today = new Date();
+  const url_filters = [
+    ['start_time', aMonthAgo(today).toISOString().substring(0, 10)],
+    ['end_time', today.toISOString().substring(0, 10)],
+    ['per_page', 5000]
+  ];
 
-    var url = base_url;
-    for (var i = 0; i < url_filters.length; i++) {
-      var sep = '&';
-      if (i == 0) { sep = '?' }
-      url += sep + url_filters[i][0] + '=' + url_filters[i][1];
-    }
+  var url = base_url;
+  for (var i = 0; i < url_filters.length; i++) {
+    var sep = '&';
+    if (i == 0) { sep = '?' }
+    url += sep + url_filters[i][0] + '=' + url_filters[i][1];
+  }
 
-    const { data: bikeLaneReports } = await axios.get(url);
+  console.log(url);
+  const { data: bikeLaneReports } = await axios.get(url);
 
-    const formattedData = {
-      "type": "FeatureCollection",
-      "features": bikeLaneReports.data.map((report, i) => ({
+  const formattedData = {
+    "type": "FeatureCollection",
+    "features": bikeLaneReports.data.map((report, i) => {
+      var deets = "";
+      if (report.description != null) {
+        deets = report.description;
+      }
+
+      return ({
         type: "Feature",
         geometry: {
           type: "Point",
           coordinates: [report.long, report.lat],
         },
         properties: {
-          title: `Point${i}`,
-          icon: "harbor",
-          color: "#FFFFFF"
+          id: report.service_request_id,
+          time: report.requested_datetime,
+          supervisor_district: report.supervisor_district,
+          neighborhood: report.neighborhoods_sffind_boundaries,
+          details: deets,
+          media: report.media_url
         }
-      }))
-    };
+      })
+    })
+  };
 
-    return formattedData;
-  }  // fetch311
+  return formattedData;
+}  // fetch311
