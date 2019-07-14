4b8c5116cd3195fe596ae83da7643761f493ce51
Alex Gaesser <agaesser@gmail.com>
2019-07-14 14:49:49 -0700

    updated some text, added some icons

--- src/backend/middlewares.mjs
+++ src/backend/middlewares.mjs
@@ -4,24 +4,26 @@ import favicon from "express-favicon";
 import hotMiddleware from "webpack-hot-middleware";
 
 const addProductionMiddlewares = app => {
-  app.use(favicon("./public/favicon.png"));
+  app.use(favicon("./public/images/lanebreach_favicon.png"));
   app.use(express.static(path.resolve("public/dist/")));
   app.get("/*", (_, res) =>
-    res.sendFile("index.html", { root: "./public/dist" }),
+    res.sendFile("index.html", { root: "./public/dist" })
   );
   return app;
 };
 
 const addDevMiddlewares = app => {
-  app.use(favicon(path.join(__dirname, "../../public/favicon.png")));
+  app.use(
+    favicon(path.join(__dirname, "../../public/images/lanebreach_favicon.png"))
+  );
   app.use(express.static(path.join(__dirname, "../../public")));
   app.use(hotMiddleware);
   return app;
 };
 
 const addMiddlewares = app => {
-  if(process.env.NODE_ENV === "production") {
-      return addProductionMiddlewares(app)
+  if (process.env.NODE_ENV === "production") {
+    return addProductionMiddlewares(app);
   }
   return addDevMiddlewares(app);
 };
