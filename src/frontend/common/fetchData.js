ae86f9b5e9b641b3b8091d1653b1b666a9b6c113
Pavel Krajcevski <Krajcevski@gmail.com>
2019-07-31 19:41:14 -0700

    Change API request to get a year's worth of data to make slider more interesting

--- src/frontend/common/fetchData.js
+++ src/frontend/common/fetchData.js
@@ -6,13 +6,19 @@ function aMonthAgo(date) {
   return new Date(date - (day1 - day2));
 }
 
+function aYearAgo(date) {
+  const day1 = Date.parse('25 Aug 2018 00:00:00 GMT')
+  const day2 = Date.parse('25 Aug 2017 00:00:00 GMT')
+  return new Date(date - (day1 - day2));
+}
+
 export async function fetch311() {
   const base_url = 'https://lane-breach.herokuapp.com/api/sf311_cases'
   var today = new Date();
   const url_filters = [
-    ['start_time', aMonthAgo(today).toISOString().substring(0, 10)],
+    ['start_time', aYearAgo(today).toISOString().substring(0, 10)],
     ['end_time', today.toISOString().substring(0, 10)],
-    ['per_page', 5000]
+    ['per_page', 100000]
   ];
 
   var url = base_url;
